d0c72733f19ec2acfed6796f55c0f07ef1e34ff2
Peyam Zangana <peyamz@yahoo.com>
2018-10-02 11:20:48 -0500

    assignment 5 start

---
+++ assignment-5/scripts.js
@@ -0,0 +1,51 @@
+/* Package data and constructor objects */
+
+// Package data array (simulated data source, such as JSON or database recordset)
+var data = [
+  {
+    name: 'emmet',
+    description: 'Emmet is the number one code snippet tool.'
+    author: 'emmetio',
+    url: 'https://atom.io/packages/emmet',
+    downloads: 1662209
+    stars: 2534,
+    selector: 'p1'
+  },
+  {
+    name: 'atom-beautify',
+    description: 'The atom-beautify package will clean up your code, and make everything more readable!'
+    author: 'Glavin001',
+    url: 'https://atom.io/packages/atom-beautify',
+    downloads: 4228040,
+    stars: 4541,
+    selector: 'p2'
+  },
+  {
+    name:
+    description:
+    author:
+    url:
+    downloads:
+    stars:
+    selector: 'p3'
+  },
+  {
+    name:
+    description:
+    author:
+    url:
+    downloads:
+    stars:
+    selector: 'p4'
+  },
+  {
+    name:
+    description:
+    author:
+    url:
+    downloads:
+    stars:
+    selector: 'p5'
+  },
+
+]
